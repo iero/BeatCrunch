ffe0082ae433f5e558adbb05dde5b151755953f5
Greg Fabre <iero@users.noreply.github.com>
2017-07-18 15:22:30 +0200

    update HOME

--- dictionaries/update.bash
+++ dictionaries/update.bash
@@ -2,8 +2,8 @@
 
 declare -a arr=("french_custom" "english_custom" "plain_words" "hashtags")
 
-input="/Users/greg/Code/BeatCrunch/dictionaries"
-output="/Users/greg/nltk_data/corpora/stopwords"
+input="$HOME/Code/BeatCrunch/dictionaries"
+output="$HOME/nltk_data/corpora/stopwords"
 
 for a in "${arr[@]}" ; do
   echo "Updating $a"
